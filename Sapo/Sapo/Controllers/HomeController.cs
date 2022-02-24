3eb7495fbe5f9b79382ba0a3ea03f9a957639547
benrc14 <benrc@byu.edu>
2022-02-23 22:48:24 -0700

    Controller

--- Sapo/Sapo/Controllers/HomeController.cs
+++ Sapo/Sapo/Controllers/HomeController.cs
@@ -6,32 +6,66 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Sapo.Models;
+using Sapo.Models.ViewModels;
 
 namespace Sapo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private ISapoRepository repo;
+
+        public HomeController(ISapoRepository temp)
         {
-            _logger = logger;
+            repo = temp;
         }
 
-        public IActionResult Index()
+        //private Context context {get; set;}
+
+        //public HomeController (SapoContext temp)
+        //{
+        //context = temp;
+        //}
+
+
+        public IActionResult Index(string category, int pageNum = 1)
         {
-            return View();
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .Where(b => b.Category == category || category == null)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks =
+                        (category == null
+                        ? repo.Books.Count()
+                        : repo.Books.Where(x => x.Category == category).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+
+            return View(x);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
+
+
+//[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//public IActionResult Error()
+//{
+//    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//}
