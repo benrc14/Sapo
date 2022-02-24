60fb7f5dc2e51855bc25b010adbe26b24fb00de2
benrc14 <benrc@byu.edu>
2022-02-23 22:46:56 -0700

    Component File

---
+++ Sapo/Sapo/Components/TypesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Sapo.Models;
+
+namespace Sapo.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        private ISapoRepository repo { get; set; }
+
+        public TypesViewComponent (ISapoRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData?.Values["category"];
+
+            var types = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+                
+
+            return View(types);
+        }
+
+    }
+}
