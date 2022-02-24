39863fd2748dbc693139cdca454e23590ff95835
benrc14 <benrc@byu.edu>
2022-02-23 22:44:32 -0700

    Initial

---
+++ Sapo/Sapo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sapo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
